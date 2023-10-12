e2459234101b93eb8e7456adc4a590147589d154
mohammed <mohammed.salad@student.moringaschool.com>
2023-10-12 08:01:36 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const getMain=document.getElementById("main").remove();
+const newHeader = document.createElement('h1')
+document.body.append(newHeader)
+newHeader.id=victory
